0b372d61f0578fe4d07b02b04b9bcfc0cf50dc76
Quotion <infinityhorizon@mail.ru>
2021-09-07 16:13:11 +0300

    update images

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 def embeds():
     return None
-=======
-def embeds():
-    return None
->>>>>>> 75b13045fc71e61cb736d610ba0be5d6d89e8926
--- modueles/help.py
+++ modueles/help.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import discord
 from discord.ext import commands
 from models import *
@@ -16,23 +15,4 @@ class MainCommands(commands.HelpCommand, name="Помощь"):
 
         self.choice_on = False
 
-=======
-import discord
-from discord.ext import commands
-from models import *
-
-
-class MainCommands(commands.HelpCommand, name="Помощь"):
-
-    def __init__(self, client):
-        self.client = client
-
-        logger = logging.getLogger("main_commands")
-        logger.setLevel(logging.INFO)
-
-        self.logger = logger
-
-        self.choice_on = False
-
->>>>>>> 75b13045fc71e61cb736d610ba0be5d6d89e8926
         self.generate_promo.start()
