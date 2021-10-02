67b5ad6431052923e07ef1ebe31840dbb793d2a8
Quotion <infinityhorizon@mail.ru>
2021-10-02 21:59:39 +0300

    permissions update

--- main.py
+++ main.py
@@ -331,7 +331,7 @@ class Katherine(object):
                 message = await channel.fetch_message(json["madadev"]["message_advert"])
 
                 if payload.message_id != json["madadev"]["message_advert"] or \
-                        not payload.member.permissions_in(request_channel).administrator:
+                        not payload.member.permissions_in(request_channel).manage_channels:
                     return
 
                 role_access = guild.get_role(json["madadev"]["join_role"])
